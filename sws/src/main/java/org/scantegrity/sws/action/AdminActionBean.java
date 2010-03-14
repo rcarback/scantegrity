09b57c6a251ebf09b6ae4a2d36675619b36c2f16
rick <rick@29ff4f17-4be9-44af-a7df-7a8342e46c90>
2010-03-14 23:36:54 +0000

    Maven enabled web project.
    git-svn-id: svn+ssh://tammany.cs.umbc.edu/web/svn/trunk@556 29ff4f17-4be9-44af-a7df-7a8342e46c90

---
+++ sws/src/main/java/org/scantegrity/sws/action/AdminActionBean.java
@@ -0,0 +1,70 @@
+package org.scantegrity.sws.action;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import net.sourceforge.stripes.action.DefaultHandler;
+import net.sourceforge.stripes.action.ForwardResolution;
+import net.sourceforge.stripes.action.HandlesEvent;
+import net.sourceforge.stripes.action.Resolution;
+import net.sourceforge.stripes.action.RedirectResolution;
+
+public class AdminActionBean extends RestrictedActionBean {
+	//Parameters for database connection
+	private static final String c_dbAddress = "jdbc:derby:";
+	private static final String c_dbName = "TPDB";
+	private static final String c_dbUser = "APP";
+	private static final String c_dbPass = "";
+	
+	@HandlesEvent(value="logout")
+	public Resolution logout()
+	{
+		c_ctx.getRequest().getSession().invalidate();
+		return new RedirectResolution(c_ctx.getServletContext().getContextPath());
+	}
+	
+	@HandlesEvent(value="deleteDatabase")
+	public Resolution deleteDatabase()
+	{
+		try
+		{
+			//Load derby database driver
+			Class.forName("org.apache.derby.jdbc.ClientDriver").newInstance();
+			//Create connection to database.  Create database if it doesn't exist.
+			Connection l_conn = DriverManager.getConnection(c_dbAddress + c_dbName + ";create=true;" + "user=" + c_dbUser + ";password=" + c_dbPass);
+	
+			//Create SQL statement object
+			Statement l_query = l_conn.createStatement();
+		
+
+			l_query.executeUpdate("DELETE FROM ContestResults");
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		finally{}
+		
+		return new ForwardResolution("/WEB-INF/pages/admin.jsp");
+	}
+	
+	@DefaultHandler
+	public Resolution submit()
+	{
+		Resolution l_userCheck = super.checkUser();
+		if( l_userCheck != null ) return l_userCheck;
+		
+		return new ForwardResolution("/WEB-INF/pages/admin.jsp");
+	}
+
+}
---
+++ sws/src/main/webapp/WEB-INF/pages/contact-success.jsp
@@ -0,0 +1,18 @@
+<%@ taglib prefix="stripes" uri="http://stripes.sourceforge.net/stripes.tld" %>
+
+<stripes:layout-render name="/WEB-INF/layout/default.jsp">
+    <stripes:layout-component name="contents">
+    <h1>Thank You!</h1>
+    
+    <p>Your message has been sent successfully!</p>
+    
+    <p>We will respond to your message within 2 days. If you have not receives a response, or are unhappy with the response received, please contact the <a href="http://takomaparkmd.gov/clerk">City Clerk</a>.</p>
+    
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    </stripes:layout-component>
+</stripes:layout-render>    
+    
